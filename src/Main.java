f4bf0a43eae7983dc94f0243a28f6f523c7035be
theberdakh <theberdakh@gmail.com>
2024-06-22 17:04:39 +0500

    add playground

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,30 @@
+import java.util.Random;
+
 public class Main {
 
-
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+       askGeneralJavaQuestions();
+    }
+
+   static void askGeneralJavaQuestions() {
+        Questions questions = new Questions();
+        Random random =new Random();
+        int randomQuestionIndex = random.nextInt(questions.questions.length-1);
+        System.out.println(questions.questions[randomQuestionIndex]);
+    }
+
+    void askIntermediateJavaQuestions() {
+        Questions questions = new Questions();
+        Random random =new Random();
+        int randomQuestionIndex = random.nextInt(questions.intermediateQuestions.length-1);
+        System.out.println(questions.questions[randomQuestionIndex]);
+    }
+
+    void askExpertJavaQuestions() {
+        Questions questions = new Questions();
+        Random random =new Random();
+        int randomQuestionIndex = random.nextInt(questions.expertQuestions.length-1);
+        System.out.println(questions.questions[randomQuestionIndex]);
+
     }
 }
